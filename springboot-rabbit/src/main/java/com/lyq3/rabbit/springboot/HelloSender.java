b98caa984e46d68ec20cbb355b922c792e15d42f
lyq3 <99190092@qq.com>
2018-05-30 21:35:59 +0800

    SpringBoot集成RabbitMQ

--- springboot-rabbit/src/main/java/com/lyq3/rabbit/springboot/HelloSender.java
+++ springboot-rabbit/src/main/java/com/lyq3/rabbit/springboot/HelloSender.java
@@ -2,9 +2,10 @@ package com.lyq3.rabbit.springboot;
 
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
-
+@Component
 public class HelloSender {
     @Autowired
     private AmqpTemplate rabbitTemplate;
